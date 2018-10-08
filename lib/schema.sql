1fe5ebdfdb080e98dfe2d84ee286507efb54eddb
Robert Snyder <r.snyder49@gmail.com>
2018-10-08 19:43:03 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,7 +1,16 @@
 CREATE TABLE series (
   id INTEGER PRIMARY KEY, 
   title TEXT, 
-  author TEXT, 
-  sub_genre TEXT
+  author_id INTEGER, 
+  sub_genre_id INTEGER
 );
 
+CREATE TABLE sub_genre (
+  id INTEGER PRIMARY KEY, 
+  name TEXT
+);
+
+CREATE TABLE authors (
+  id INTEGER PRIMARY KEY, 
+  name TEXT
+);
