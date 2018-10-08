21b017534e286a049c61fac0a6b891122fe75559
Robert Snyder <r.snyder49@gmail.com>
2018-10-08 20:18:26 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -24,7 +24,7 @@ def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name
   FROM authors 
   JOIN series ON series.author_id == author_id 
-  JOIN subgenres "
+  JOIN subgenres ON subgenres.id"
 end
 
 def select_series_title_with_most_human_characters
