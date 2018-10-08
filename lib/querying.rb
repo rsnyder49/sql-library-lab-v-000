6b24693722ca7b4028525c7c246d3d02336927a1
Robert Snyder <r.snyder49@gmail.com>
2018-10-08 20:10:26 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,8 @@ def select_value_and_count_of_most_prolific_species
   "SELECT characters.species 
   COUNT(*) 
   FROM charcters 
-  GROUP BY species "
+  GROUP BY species 
+  ORDER BY COUNT(species)"
 end
 
 def select_name_and_series_subgenres_of_authors
