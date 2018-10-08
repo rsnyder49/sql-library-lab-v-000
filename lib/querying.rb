da684f717287347bc57b5f921a3a05c8c05e98a5
Robert Snyder <r.snyder49@gmail.com>
2018-10-08 20:12:06 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -15,7 +15,7 @@ end
 def select_value_and_count_of_most_prolific_species
   "SELECT characters.species 
   COUNT(*) 
-  FROM charcters 
+  FROM characters 
   GROUP BY species 
   ORDER BY COUNT(species) DESC LIMIT 1"
 end
