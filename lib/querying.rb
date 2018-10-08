e0d7421c3d58b914af83652950086239d6aff1ea
Robert Snyder <r.snyder49@gmail.com>
2018-10-08 20:23:00 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -34,12 +34,12 @@ def select_series_title_with_most_human_characters
   JOIN character_books ON character_books.book_id == books.id 
   JOIN characters ON character_books.character_id == characters.id 
   WHERE characters.species == 'human' 
-  GROUP BY series.title ORDER BY COUNT(*) DESC LIMIT 1"
+  GROUP BY series.title ORDER BY COUNT(*) DESC LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, COUNT(*) as book_count from character_books 
   JOIN characters ON character_books.character_id = characters.id 
   GROUP BY characters.name 
-  ORDER BY book_count DESC"
+  ORDER BY book_count DESC;"
 end
